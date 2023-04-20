844c555e34968b4acca70864ed84e8f9a1c587fe
gaulbaram0111 <gaulbaram0111@gmail.com>
2023-04-20 23:47:50 +0900

    230420

---
+++ ch02/ch02_4/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  braketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: "none",
+  arrowParens: "avoid",
+  semi: false,
+  printwidth: 90,
+};
---
+++ ch02/ch02_4/src/App.tsx
@@ -0,0 +1,8 @@
+import './App.css'
+
+export default function App() {
+  const texts = ['hello', 'world'].map((text, index) => (
+    <p key={index} children={text} />
+  ))
+  return <div children={texts} />
+}
