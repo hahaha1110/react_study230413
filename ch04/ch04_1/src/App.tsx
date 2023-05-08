d8e40562e384429fdccc909a1a9676f3b4180463
gaulbaram0111 <gaulbaram0111@gmail.com>
2023-05-09 02:13:56 +0900

    230509

--- ch04/ch04_1/src/App.tsx
+++ ch04/ch04_1/src/App.tsx
@@ -1,6 +1,7 @@
+import { useClock } from './hooks'
 import Clock from './pages/Clock'
 
 export default function App() {
-  let today = new Date()
+  const today = useClock()
   return <Clock today={today} />
 }
