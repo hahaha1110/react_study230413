ca78b4077958e99f587bd7ceea7a4fde362a0282
gaulbaram0111 <gaulbaram0111@gmail.com>
2023-05-10 02:42:19 +0900

    230510

--- ch04/ch04_2/src/pages/Memo.tsx
+++ ch04/ch04_2/src/pages/Memo.tsx
@@ -1,8 +1,49 @@
-export default function CopyMe() {
+import { useMemo } from 'react'
+import { Title, Avatar } from '../components'
+import * as D from '../data'
+
+export default function CreateOrUseTest() {
+  // prettier-ignore
+  const headTexts = useMemo<string[]>(() => [
+    'No.', 'Name', 'Job Title', 'Email Adress'
+  ], [])
+  const users = useMemo<D.IUser[]>(
+    () => D.makeArray(100).map(D.makeRandomUser),
+    []
+  )
+
+  const head = useMemo(
+    () => headTexts.map(text => <th key={text}>{text}</th>),
+    [headTexts]
+  )
+
+  const body = useMemo(
+    () =>
+      users.map((user, index) => (
+        <tr key={user.uuid}>
+          <th>{index + 1}</th>
+          <td className="flex items-center">
+            <Avatar src={user.avatar} size="1.5rem" />
+            <p className="ml-2">{user.name}</p>
+          </td>
+          <td>{user.jobTitle}</td>
+          <td>{user.email}</td>
+        </tr>
+      )),
+    [users]
+  )
+
   return (
-    <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">CopyMe</h2>
-      <div className="mt-4"></div>
-    </section>
+    <div className="mt-4">
+      <Title>CreateOrUseTest</Title>
+      <div className="overflow-x-auto mt-4 p-4">
+        <table className="table table-zebra table-compact w-full">
+          <thead>
+            <tr>{head}</tr>
+          </thead>
+          <tbody>{body}</tbody>
+        </table>
+      </div>
+    </div>
   )
 }
