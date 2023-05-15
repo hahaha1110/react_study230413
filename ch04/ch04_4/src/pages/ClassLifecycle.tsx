b150fbd3ef6116ce2766cb61ae16edf31c37beb4
gaulbaram0111 <gaulbaram0111@gmail.com>
2023-05-16 01:23:08 +0900

    230516

--- ch04/ch04_4/src/pages/ClassLifecycle.tsx
+++ ch04/ch04_4/src/pages/ClassLifecycle.tsx
@@ -1,8 +1,34 @@
-export default function CopyMe() {
-  return (
-    <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">CopyMe</h2>
-      <div className="mt-4"></div>
-    </section>
-  )
+import { Component } from 'react'
+import { Title } from '../components'
+
+export default class ClassLifecycle extends Component {
+  state = {
+    today: new Date(),
+    intervalId: null as unknown as NodeJS.Timer // 타입스크립트가 요구하는 구현방식
+  }
+
+  componentDidMount() {
+    const duration = 1000
+    const intervalId = setInterval(
+      () => this.setState({ today: new Date() }),
+      duration
+    )
+    this.setState({ intervalId })
+  }
+  componentWillUnmount() {
+    clearInterval(this.state?.intervalId)
+  }
+
+  render() {
+    const { today } = this.state
+    return (
+      <section className="mt-4">
+        <Title>ClassLifecycle</Title>
+        <div className="flex flex-col items-center mt-4">
+          <p className="font-mono text-3xl">{today.toLocaleTimeString()}</p>
+          <p className="font-mono text-3xl">{today.toLocaleDateString()}</p>
+        </div>
+      </section>
+    )
+  }
 }
