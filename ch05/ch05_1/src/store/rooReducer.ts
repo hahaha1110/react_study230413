526da6bef224a2dd322f0f367e54421debc6790d
gaulbaram0111 <gaulbaram0111@gmail.com>
2023-05-21 23:29:03 +0900

    230521

---
+++ ch05/ch05_1/src/store/rooReducer.ts
@@ -0,0 +1,10 @@
+import type { Action } from 'redux'
+import { AppState } from './AppState'
+
+const initialAppState = {
+  today: new Date()
+}
+export const rootReducer = (
+  state: AppState = initialAppState,
+  action: Action
+) => state
