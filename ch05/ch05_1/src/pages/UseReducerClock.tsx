8d95eb0d6a3116562e8233e7dc97da9e970e84a6
gaulbaram0111 <gaulbaram0111@gmail.com>
2023-05-23 16:52:41 +0900

    230523

--- ch05/ch05_1/src/pages/UseReducerClock.tsx
+++ ch05/ch05_1/src/pages/UseReducerClock.tsx
@@ -1,11 +1,19 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useReducer } from 'react'
+import { setTodayAction } from '../store/actions'
 import { AppState } from '../store'
 import { Div, Title, Subtitle } from '../components'
 import { useInterval } from '../hooks'
 
 export default function ReduxClock() {
-  const today = useSelector<AppState, Date>(state => state.today)
-  const dispatch = useDispatch()
+  const [{ today }, dispatch] = useReducer(
+    (state: AppState, action: setTodayAction) => {
+      switch (action.type) {
+        case 'setToday':
+          return { ...state, today: new Date() }
+      }
+    },
+    { today: new Date() }
+  )
 
   useInterval(() => {
     dispatch({ type: 'setToday', today: new Date() })
@@ -13,7 +21,8 @@ export default function ReduxClock() {
 
   return (
     <Div className="flex flex-col items-center justify-center h-screen text-white bg-primary">
-      <Title className="text-5xl">{today.toLocaleTimeString()}</Title>
+      <Title className="text-5xl">UseReducerClock</Title>
+      <Title className="mt-4 text-3xl">{today.toLocaleTimeString()}</Title>
       <Subtitle className="mt-4 text-2xl">
         {today.toLocaleDateString()}
       </Subtitle>
