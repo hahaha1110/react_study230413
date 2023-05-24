310d9945d3fb2e19bdfc559b5c1c7211a60bd90a
gaulbaram0111 <gaulbaram0111@gmail.com>
2023-05-25 03:01:04 +0900

    230525

--- ch05/ch05_2/src/store/counter/actions.ts
+++ ch05/ch05_2/src/store/counter/actions.ts
@@ -1,1 +1,8 @@
 import type * as T from './types'
+
+export const setCounter = (payload: T.State): T.SetCounterAction => ({
+  type: '@counter/setCounter',
+  payload
+})
+export const increaseConter = () => setCounter(1)
+export const decreaseConter = () => setCounter(-1)
