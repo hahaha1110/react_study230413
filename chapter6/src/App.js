d508440b97e48ded54f32252a1a698a8ebe2cfdc
gaulbaram0111 <gaulbaram0111@gmail.com>
2023-07-03 11:48:17 +0900

    230703

--- chapter6/src/App.js
+++ chapter6/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Controller";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  const countHandler = (e) => {
+    setCount(Number(count) + Number(e.target.innerText));
+  };
+
+  const textHandler = (e) => {
+    setText(e.target.value);
+  };
+
+  const didMountRef = useRef(false);
+
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true;
+      return;
+    } else {
+      console.log("컴포넌트 업데이트!");
+    }
+    const intervalId = setInterval(() => {
+      console.log("깜빡");
+    }, 1000);
+
+    return () => {
+      console.log("클린업");
+      clearInterval(intervalId);
+    };
+  });
+
+  useEffect(() => {
+    console.log("컴포넌트 마운트 :::");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count={count} />
+      </section>
+      <section>
+        <input value={text} onChange={textHandler} />
+      </section>
+      <section>
+        <Controller countHandler={countHandler} />
+      </section>
     </div>
   );
 }
